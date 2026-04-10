936957d3205a036b7604d069a55300fea83623f2
aria21x <silmaromarianne1@gmail.com>
2026-04-10 23:25:15 +0800

    Update settings.py

--- core/config/settings.py
+++ core/config/settings.py
@@ -2,7 +2,7 @@ import os
 
 API_PORT = os.getenv('API_PORT', '8000')
 
-settings = Settings()
+
 if API_PORT == '':
     API_PORT = '8000'
 else:
@@ -14,6 +14,6 @@ class Settings:
     """Dynamic settings loader"""
     def __getattr__(self, name):
         return os.getenv(name)
-
+settings = Settings()
 
 # Use the API_PORT in your application as needed
