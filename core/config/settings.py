f63b0ff0cff6be8b32d9762403bc254f35fb1dc0
aria21x <silmaromarianne1@gmail.com>
2026-04-10 23:28:59 +0800

    Update settings.py

--- core/config/settings.py
+++ core/config/settings.py
@@ -1,19 +1,15 @@
+# At the very end of core/config/settings.py
 import os
-
-API_PORT = os.getenv('API_PORT', '8000')
-
-
-if API_PORT == '':
-    API_PORT = '8000'
-else:
-    API_PORT = os.getenv('PORT', API_PORT)
+from dotenv import load_dotenv
 
 load_dotenv()
 
 class Settings:
-    """Dynamic settings loader"""
+    """Dynamic settings loader for Railway compatibility"""
     def __getattr__(self, name):
         return os.getenv(name)
-settings = Settings()
+    
+    def get(self, name, default=None):
+        return os.getenv(name, default)
 
-# Use the API_PORT in your application as needed
+settings = Settings()
